8e6e8fa22baf87061da9adca8159412247022001
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-16 11:17:05 +0530

    Initial Commit

---
+++ SwiftUILocalization/SwiftUILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalizationApp.swift
+//  SwiftUILocalization
+//
+//  Created by Knoxpo MacBook Pro on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
